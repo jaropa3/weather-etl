b31b384b7e5cc8d162d600555df408e6aa54796a
jaropa3 <jarek.opasinski@gmail.com>
2026-04-28 20:00:06 +0200

    changes

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
 	run()
 
 
-#1. zapis do bazy na gcp i cron
+#1. zapis do bazy na gcp i cronkusia
 #1a. zapytania sql
 #2. Krok po kroku w konsoli GCP
 #3. zad2. buduj warsty raw -> stag -> final
